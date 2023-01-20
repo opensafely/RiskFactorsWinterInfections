b4511993a39fde7f597fde04278bd5741be613c0
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-01-20 10:42:23 +0000

    Make analysis code directory and output code directory match

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -98,14 +98,14 @@ actions_list <- splice(
     )
   ),
   
-  comment("Data preprocess and cleaning"),
+  comment("Data cleaning"),
   
   action(
-    name = "data_preprocess_cleaning_winter2019",
-    run = "r:latest analysis/cleaning/data-cleaning.R winter2019",
+    name = "data_cleaning_winter2019",
+    run = "r:latest analysis/data_cleaning/data_cleaning.R winter2019",
     needs = list("generate_study_population_winter2019"),
     moderately_sensitive = list(
-      describe = glue("output/describe_input_prevax_*.txt")
+      describe = glue("output/data_cleaning/describe_input_winter2019.txt")
     ),
     highly_sensitive = list(
       cohort = glue("output/input_winter2019.csv.gz")
@@ -113,11 +113,11 @@ actions_list <- splice(
   ), 
   
   action(
-    name = "data_preprocess_cleaning_winter2021",
-    run = "r:latest analysis/cleaning/data-cleaning.R winter2021",
+    name = "data_cleaning_winter2021",
+    run = "r:latest analysis/data_cleaning/data_cleaning.R winter2021",
     needs = list("agenerate_study_population_winter2021"),
     moderately_sensitive = list(
-      describe = glue("output/describe_input_prevax_*.txt")
+      describe = glue("output/data_cleaning/describe_input_winter2021.txt")
     ),
     highly_sensitive = list(
       cohort = glue("output/input_winter2021.csv.gz")
