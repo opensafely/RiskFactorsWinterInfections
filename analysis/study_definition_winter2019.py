fd5d5153e61ec7071debf497c2484a57f8fe7c36
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2023-01-30 11:34:07 +0000

    change index date to index date variable

--- analysis/study_definition_winter2019.py
+++ analysis/study_definition_winter2019.py
@@ -27,7 +27,7 @@ import study_definition_helper_functions as helpers
 from common_variables import generate_common_variables
 (
     dynamic_variables
-) = generate_common_variables(index_date_variable="2019-12-01", index_date_variable_covariates = "2019-11-30" )
+) = generate_common_variables(index_date_variable="index_date_winter2019", index_date_variable_covariates = "2019-11-30" )
 
 
 study = StudyDefinition(
@@ -39,19 +39,19 @@ study = StudyDefinition(
     },
 
     # Specify index date for study
-    index_date = "2019-12-01",
+    index_date_winter2019 = "2019-12-01",
 
     # Define the study population 
     # NB: not all inclusions and exclusions are written into study definition
     population = patients.satisfying(
         "registered",
-        registered=patients.registered_as_of("index_date"),
+        registered=patients.registered_as_of("index_date_winter2019"),
     ),
         
     ### Covariates and inclusion/exclusion variables
     ## Patient alive at index date
     has_died = patients.died_from_any_cause(
-        on_or_before = "index_date",
+        on_or_before = "index_date_winter2019",
         returning = "binary_flag",
     ),
    
